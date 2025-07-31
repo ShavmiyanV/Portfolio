3442e242ce352f8cebb2b64000b9fd046ddf1add
Shavmiyan <shavmiyan16@gmail.com>
2025-07-31 23:21:08 +0530

    update: [Complete About Me Section]

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import AboutMeMain from "./components/aboutMeSection/AboutMeMain";
 import HeroGradient from "./components/heroSection/HeroGradient";
 import HeroMain from "./components/heroSection/HeroMain";
 import SubHeroSection from "./components/heroSection/SubHeroSection";
@@ -9,6 +10,7 @@ function App() {
       <HeroMain/>
       <HeroGradient/>
       <SubHeroSection/>
+      <AboutMeMain/>
   </main>
 }
 
---
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const AboutMeText = () => {
+  return (
+    <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
+      <h2 className="text-6xl text-cyan mb-10">About Me</h2>
+      <p className="text-white">
+        Hi, I'm Shavmiyan, an enthusiastic and dedicated undergraduate student
+        at the University of Moratuwa, pursuing a BSc (Hons) in Information
+        Technology. I’m passionate about software development, UI/UX design, and
+        creating digital experiences that are both functional and visually
+        appealing. With a strong foundation in web technologies like React.js,
+        Tailwind CSS, and modern design tools such as Figma and Photoshop, I
+        enjoy turning ideas into user-friendly applications. I'm always eager to
+        learn, take on new challenges, and grow as a developer while
+        contributing meaningfully to real-world projects.
+      </p>
+      <button className="border border-orange rounded-full py-2 px-4 test-lg flex items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-cyan">My Projects</button>
+    </div>
+  );
+};
+
+export default AboutMeText;
